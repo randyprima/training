414fc0ca4f617e60a402b94a875be48f12aa62c1
randy prima brahmantara <randy.prima@lapan.go.id>
2020-07-28 15:32:47 +0700

    Update calculateCloud_FromPolygon.py
    update zonal statistics dan gdalrasterize menggunakan gdal api

--- calculateCloud_FromPolygon.py
+++ calculateCloud_FromPolygon.py
@@ -26,52 +26,36 @@ def postgres_test(hostname,db_name, username, pascode):
 def multi_run_wrapper2(args):
    return ingest(*args)
 
-def ingest(hostname,db_name,username,db_table,pascode,short_name,yy_start,xx_start,tilesize,ul_latitude,ul_longitude,result_raster,smetadata,incl,incx,date,geometric,spectral,rmse,rndval):
+def ingest(hostname,db_name,username,db_table,pascode,short_name,yy_start,xx_start,tilesize,ul_latitude,ul_longitude,result_raster,smetadata,incl,incx,date,geometric,spectral,rmse,rndval,geotransform):
     conn = psycopg2.connect(host=hostname, database=db_name, user=username, password=pascode)
     c = conn.cursor()
     lr_latitude=round(ul_latitude-tilesize,rndval)
     lr_longitude=round(ul_longitude+tilesize,rndval)
-    if str(ul_latitude)[0]=="-":
-        lat=str(ul_latitude).replace("-","S").replace(".","")
-    else:
-        lat='N'+str(ul_latitude).replace(".","")
+            
+    originX = geotransform[0]
+    originY = geotransform[3]
+    pixelWidth = geotransform[1]
+    pixelHeight = geotransform[5]
     
-    if len (lat) >= 5:
-        new_lat = lat[:1]+'0'+lat[1:]
-    elif len(lat) == 4:
-        new_lat = lat[:1]+'00'+lat[1:]
-    elif len(lat) == 3:
-        new_lat = lat[:1]+'000'+lat[1:]
-    else:
-        new_lat = lat
+    # Specify offset and rows and columns to read
+    xoff = int((ul_longitude - originX)/pixelWidth)
+    yoff = int((originY - ul_latitude)/pixelWidth)
+    xcount = int(round((lr_longitude - ul_longitude)/pixelWidth))
+    ycount = int(round(abs((ul_latitude - lr_latitude)/pixelWidth)))
     
-    lon = 'E'+str(ul_longitude).replace(".","")
-    if len (lon) >= 5:
-        new_lon = lon[:1]+'0'+lon[1:]
-    else:
-        new_lon = lon
-        
-    nama_tile=new_lat+"_"+new_lon
-    
-    res_raster_clip = result_raster.replace('.tif','_'+nama_tile+'_clip.vrt')
-        
-    raster = gdal.Warp(res_raster_clip, result_raster, dstSRS='EPSG:4326', format='VRT', outputBounds=(ul_longitude,ul_latitude,lr_longitude,lr_latitude))
-    
-    banddataraster = raster.GetRasterBand(1)
-    dataraster = banddataraster.ReadAsArray().astype(np.uint8)
+    with rasterio.open(result_raster) as src:
+        dataraster = src.read(1, window=Window(xoff, yoff, xcount, ycount))
 
     # nodata = (dataraster==1).sum()
-    clear = (dataraster==2).sum()
+    clear = (dataraster==1).sum()
     cloud = (dataraster==101).sum()
     pix_count = np.size(dataraster)
-    ndata = round((((clear+cloud)/pix_count)*100),2)
-    #nclear = round(clear/pix_count,2)
-    ncloud = round(cloud/pix_count*100,2)
-    raster = None
-    os.remove(res_raster_clip)
-    c.execute(sql.SQL("INSERT INTO {} (sceneid, ullon_scene, ullat_scene, tile_size, ul_latitude, ul_longitude, ndata, pixdata, ncloud, metadata, inc_along, inc_across, img_date, geometric, spectral, ce90) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)").format(sql.Identifier(db_table)), (short_name,yy_start,xx_start,tilesize,ul_latitude,ul_longitude,ndata,pix_count,ncloud,metadata,incl,incx,date,geometric,spectral,rmse))
-    conn.commit()
-    conn.close()
+    if pix_count != 0:
+        ndata = round((((clear+cloud)/pix_count)*100),2)
+        ncloud = round(cloud/pix_count*100,2)
+        c.execute(sql.SQL("INSERT INTO {} (sceneid, ullon_scene, ullat_scene, tile_size, ul_latitude, ul_longitude, ndata, pixdata, ncloud, metadata, inc_along, inc_across, img_date, geometric, spectral, ce90) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)").format(sql.Identifier(db_table)), (short_name,yy_start,xx_start,tilesize,ul_latitude,ul_longitude,ndata,pix_count,ncloud,metadata,incl,incx,date,geometric,spectral,rmse))
+        conn.commit()
+        conn.close()
 
 def main (hostname, db_name, username, metadata, db_table, pascode, dirtemp, tilesize, rndval):
     smetadata = os.path.basename(metadata) # DIM_SPOT6_PMS_201809040237336_ORT_SPOT6_20180904_02503513ik99j3p4hk8_1.XML    
@@ -103,7 +87,7 @@ def main (hostname, db_name, username, metadata, db_table, pascode, dirtemp, til
             		if baris.find("<ACCURACY_CE90")==0:
             			ce90.append(baris.split(">")[1].split("<")[0])
             	if len(ce90)>0:
-            		rmse=str(round(float(ce90[0]),2))
+            		rmse=str(round(float(ce90[0])*30.87,2))
             		if rmse=='0' or rmse=='0.0':
             			rmse='N/A'
             	else:
@@ -116,10 +100,31 @@ def main (hostname, db_name, username, metadata, db_table, pascode, dirtemp, til
                 geom = feature.GetGeometryRef()
                 env = geom.GetEnvelope()
             roi_raster = dirtemp+os.sep+os.path.basename(roi)[:28]+".tif"
-            os.popen("gdal_rasterize -q -a_srs EPSG:4326 -l MaskFeature -burn 1 -tr 0.0005 0.0005 -init 0 -te %f %f %f %f -ot Byte -of GTiff %s %s" %(env[0],env[2],env[1],env[3],roi,roi_raster))
+            originX = env[0]
+            originY = env[3]
+            pixelWidth = 0.0005
+            pixelHeight = -0.0005
+            rows = int(abs(round((env[3]-env[2])/pixelHeight)))
+            cols = int(round((env[1]-env[0])/pixelWidth))
             
+            driver = gdal.GetDriverByName('GTiff')
+            outRaster = driver.Create(roi_raster, cols, rows, 1, gdal.GDT_Byte)
+            outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
+            shp = ogr.Open(roi)
+            lyr = shp.GetLayer()
+            # Rasterize zone polygon to raster
+            gdal.RasterizeLayer(outRaster, [1], lyr, burn_values=[1])
+            outRaster = None
+
             cld_raster = dirtemp+os.sep+os.path.basename(cld)[:28]+".tif"  
-            os.system("gdal_rasterize -q -a_srs EPSG:4326 -l MaskFeature -burn 100.0 -tr 0.0005 0.0005 -init 1 -te %f %f %f %f -ot Byte -of GTiff %s %s" %(env[0],env[2],env[1],env[3],cld,cld_raster))
+            driver = gdal.GetDriverByName('GTiff')
+            outRaster = driver.Create(cld_raster, cols, rows, 1, gdal.GDT_Byte)
+            outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
+            shp = ogr.Open(cld)
+            lyr = shp.GetLayer()
+            # Rasterize zone polygon to raster
+            gdal.RasterizeLayer(outRaster, [1], lyr, burn_values=[100])
+            outRaster = None
             
             result_raster = cld_raster.replace("CLD","RES")
             if os.path.isfile(roi_raster) and os.path.isfile(cld_raster):
@@ -166,21 +171,17 @@ def main (hostname, db_name, username, metadata, db_table, pascode, dirtemp, til
                     for y in yy:
                         for x in xx:
                             ul_longitude=round(x,rndval)
-                            ul_latitude=round(y,rndval)
-                                        
-                            #baca database
-                            c.execute(sql.SQL("SELECT * FROM {} WHERE (sceneid =%s AND ul_latitude=%s AND ul_longitude=%s)").format(sql.Identifier(db_table)), (short_name,ul_latitude,ul_longitude))
-                            hs = c.fetchall()
-                            if hs == []: #belum ada di database, jadi bisa insert data baru di database.
-                                if short_name[:4] == 'PHR1':
-                                    ingest(hostname,db_name,username,db_table,pascode,short_name,yy_start,xx_start,tilesize,ul_latitude,ul_longitude,result_raster,smetadata,incl[1],incx[1],date,geometric,spectral,rmse,rndval)
-                                elif short_name[:4] == 'SPOT':
-                                    ingest(hostname,db_name,username,db_table,pascode,short_name,yy_start,xx_start,tilesize,ul_latitude,ul_longitude,result_raster,smetadata,incl[4],incx[4],date,geometric,spectral,rmse,rndval)
+                            ul_latitude=round(y,rndval)                            
+                            if short_name[:4] == 'PHR1':
+                                ingest(hostname,db_name,username,db_table,pascode,short_name,yy_start,xx_start,tilesize,ul_latitude,ul_longitude,result_raster,smetadata,incl[1],incx[1],date,geometric,spectral,rmse,rndval,geotransform)
+                            elif short_name[:4] == 'SPOT':
+                                ingest(hostname,db_name,username,db_table,pascode,short_name,yy_start,xx_start,tilesize,ul_latitude,ul_longitude,result_raster,smetadata,incl[4],incx[4],date,geometric,spectral,rmse,rndval,geotransform)
                     conn.close()
                     
     else:
         print('file %s tidak lengkap' %(smetadata))
         pass
+
 
 
 if __name__ == '__main__':
