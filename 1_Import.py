dfee83f994839194b4a00a8e280439a5150846d3
randy prima brahmantara <randy.prima@lapan.go.id>
2020-08-24 11:34:31 +0700

    Update 1_Import.py
    update tanpa multiprocessing

--- 1_Import.py
+++ 1_Import.py
@@ -5,33 +5,33 @@ Created on Mon Jul 13 09:21:44 2020
 @author: randy
 """
 import os, sys
-from multiprocessing import Pool
-# import tqdm
+import tqdm
 
-def multi_run_wrapper(args):
-   return runApp(*args)
-
-def runApp(app,tableNm,dirTm,metadata):
-    os.popen(app+' '+tableNm+' '+dirTm+' '+metadata)
+def runApp(app,tableNm,hostname,metadata,database,username,password,dirtem,metadata,tilesize):
+    os.popen(app+' --tbl '+tableNm+' --hos '+hostname+'  --dbn '+database+' --usr '+username+' --psw '+password+' --dir '+dirtem+' --met '+metadata+' --tzs '+tilesize)
     
 def main():
     listFiles = r'..\files\list.txt' #sys.argv[1]
     tableNm = sys.argv[1] #r'testSPOT' #sys.argv[2]
-    dirTm = r'..\temporal' #sys.argv[3]
-    if not os.path.isdir(dirTm): 
-        os.mkdir(dirTm)
+    dirtem = r'..\temporal' #sys.argv[3]
+    hostname = 'localhost'
+    database = 'test'
+    username = 'administrator'
+    password = 'teklahta@8'
+    tilesize = '0.1'
+    if not os.path.isdir(dirtem): 
+        os.mkdir(dirtem)
     app = r'python calculateCloud_FromPolygon.py '
     ins = []
     with open(listFiles, 'r') as filein:
         lines = filein.readlines()
         for line in lines:
-            ins.append((app,tableNm,dirTm,line.strip()),)
+            metadata = line.strip()
+            ins.append((app,tableNm,hostname,metadata,database,username,password,dirtem,metadata,tilesize),)
             
-    pool = Pool(1)
-    pool.map(multi_run_wrapper, ins)
-    pool.close()
-    # for _ in tqdm.tqdm(pool.imap_unordered(multi_run_wrapper, ins), total=len(ins)):
-        # pass
+    
+    for i in tqdm.tqdm(range(0,len(ins)),desc='Progress'):
+        runApp(ins[i])
     
 if __name__ == '__main__':
     main()
